3369bd3bd8dc6eeaeff59bc8cbae968486050a0a
朱文龙 <1573511441@qq.com>
2026-04-03 12:08:31 +0800

    feat: 调整配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+
+// 加载环境变量
+const env = loadEnv('', process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
   server: {
-    port: 3008,
+    port: Number(env.VITE_APP_PORT) || 8106,
     proxy: {
       '/api': {
-        target: 'http://localhost:3018',
+        target:
+          env.VITE_APP_API_BASE_URL?.replace('/api/v1', '') ||
+          'http://localhost:9201',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
       },
       '/ws': {
-        target: 'ws://localhost:3018',
+        target:
+          env.VITE_APP_WS_BASE_URL?.replace('ws://', 'http://')?.replace(
+            '/ws',
+            '',
+          ) || 'http://localhost:9201',
         ws: true,
         changeOrigin: true,
       },
