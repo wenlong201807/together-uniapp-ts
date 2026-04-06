0b68bb05327cb0d1102522dab548430cbff83f38
朱文龙 <1573511441@qq.com>
2026-04-06 09:56:53 +0800

    feat: staging dist

---
+++ dist/build/h5/assets/certification.Cq7Ivvem.js
@@ -0,0 +1,1 @@
+import{T as t}from"./index-Dll7NF33.js";const i={getTypes:()=>t.get("/certification-types"),submit:i=>t.post("/certification",i),getMyList:i=>t.get("/certification/list",{params:{status:i}}),getDetail:i=>t.get(`/certification/${i}`)};export{i as c};
