e115473fc5353de08c58b5a7644784a3137a3347
朱文龙 <1573511441@qq.com>
2026-04-12 23:27:03 +0800

    feat: 自动填充背景色

--- src/App.vue
+++ src/App.vue
@@ -38,4 +38,50 @@ body,
   min-height: 100vh;
   height: auto;
 }
+
+/* 移除浏览器自动填充时的背景色 */
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active,
+input:-internal-autofill-selected,
+.uni-input-input:-webkit-autofill,
+.uni-input-input:-webkit-autofill:hover,
+.uni-input-input:-webkit-autofill:focus,
+.uni-input-input:-webkit-autofill:active,
+.uni-input-input:-internal-autofill-selected {
+  -webkit-box-shadow: 0 0 0 1000px #fff inset !important;
+  box-shadow: 0 0 0 1000px #fff inset !important;
+  -webkit-text-fill-color: #333 !important;
+  background-color: #fff !important;
+  background-image: none !important;
+  caret-color: #333 !important;
+}
+
+/* 强制覆盖自动填充的过渡效果 */
+input,
+.uni-input-input {
+  &:-webkit-autofill {
+    -webkit-transition: background-color 0s 86400s, color 0s 86400s !important;
+    transition: background-color 0s 86400s, color 0s 86400s !important;
+  }
+
+  &:-internal-autofill-selected {
+    background-color: #fff !important;
+    background-image: none !important;
+    color: #333 !important;
+  }
+}
+
+/* uni-app H5 端 input 包裹层 */
+.uni-input-wrapper {
+  background-color: transparent !important;
+}
+
+/* 强制覆盖 uni-input 组件的自动填充样式 */
+uni-input input:-webkit-autofill,
+uni-input input:-internal-autofill-selected {
+  -webkit-box-shadow: 0 0 0 1000px #fff inset !important;
+  background-color: #fff !important;
+}
 </style>
