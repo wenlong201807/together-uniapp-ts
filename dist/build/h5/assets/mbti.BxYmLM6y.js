3155978cb819bbf862909fd9f4f87b9917a16a20
190 <123456@abcd.com>
2026-04-18 14:41:38 +0000

    feat: dist

---
+++ dist/build/h5/assets/mbti.BxYmLM6y.js
@@ -0,0 +1,1 @@
+import{a0 as t}from"./index-CzkR0KY4.js";const s={startTest:()=>t.post("/mbti/start"),submitAnswer:s=>t.post("/mbti/answer",s),submitTest:s=>t.post("/mbti/submit",s),getReport:s=>t.get("/mbti/report",{mbtiType:s}),getCurrentResult:()=>t.get("/mbti/current"),getHistory:()=>t.get("/mbti/history"),shareToSquare:()=>t.post("/mbti/share")};export{s as m};
